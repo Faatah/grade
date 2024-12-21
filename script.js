c0771a1623a8323c39c56c43b9e0a2a76980f695
Faatah <guure781@gmail.com>
2024-12-21 15:36:42 +0300

    done exercise for grade

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+let grade=85
+
+if (grade>=90){
+    console.log("You got an A grade")
+}else if(grade>=75){
+    console.log("You got an B grade")
+}else if(grade>=60){
+    console.log("You got an C grade")
+}else if(grade>=50){
+    console.log("You got an FAILED")
+}
